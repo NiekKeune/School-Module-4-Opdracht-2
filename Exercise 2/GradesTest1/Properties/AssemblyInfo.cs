41b106d8ec2021ae5e7bd6b3a06b8ae878750e14
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2018-02-15 14:48:59 +0100

    School Module 4 Opdracht 2

---
+++ Exercise 2/GradesTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradesTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradesTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("814b5383-8ccb-4b6d-92ce-04c3f8f421e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Exercise 2/GradesTest1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GradesTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestInitialize]
+        public void Init()      //Initializes the DataSource from GradesPrototype
+        {
+            GradesPrototype.Data.DataSource.CreateData();
+        }
+
+        [TestMethod]
+        public void TestValidGrade()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "1/1/2012", "Math", "A-", "Very good");
+            Assert.AreEqual(grade.AssessmentDate, "1/1/2012");
+            Assert.AreEqual(grade.SubjectName, "Math");
+            Assert.AreEqual(grade.Assessment, "A-");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadDate()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "1/1/2108", "Math", "A-", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateNotRecognized()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "13/13/2017", "Math", "A-", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadAssessment()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "1/1/2012", "Math", "F", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadSubject()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "1/1/2012", "Astrology", "A-", "Very good");
+        }
+    }
+}
